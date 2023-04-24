e45a22b24fb7a8c7c4400ccc0151ea8a1ebf6d34
Avbarkova <avbarkova@gmail.com>
2023-04-25 02:18:14 +0300

    refactor(basic-3.6): CSS property values separated by spaces after colon

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ var createNewTaskElement=function(taskString){
   var deleteButtonImg=document.createElement("img");//delete button image
 
   label.innerText=taskString;
-  label.className='task';
+  label.className="task";
 
   //Each elements, needs appending
   checkBox.type="checkbox";
@@ -45,7 +45,7 @@ var createNewTaskElement=function(taskString){
   editButton.className="edit";
 
   deleteButton.className="delete";
-  deleteButtonImg.src='./remove.svg';
+  deleteButtonImg.src="./remove.svg";
   deleteButtonImg.className="img-delete";
   deleteButton.appendChild(deleteButtonImg);
 
